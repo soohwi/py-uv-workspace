c1131f412985233b99c29033aa2e5b7c4fdfd6ba
tngnl12341 <tngnl12341@gmail.com>
2026-07-14 17:45:10 +0900

    writefile test

---
+++ main2.py
@@ -0,0 +1,12 @@
+
+# from my_shapes.area import circle_area, square_area
+
+# print("■ 커스텀 패키지 실행 결과 ■")
+# print(f"반지름 5인 원의 넓이 : {circle_area(5)}")       #출력: 78.5
+# print(f"가로4 세로5 사각형 넓이 : {square_area(4, 5)}")   #출력: 20
+
+import my_shapes.area as ma
+
+print("■ 커스텀 패키지 실행 결과 ■")
+print(f"반지름 5인 원의 넓이 : {ma.circle_area(5)}")       #출력: 78.5
+print(f"가로4 세로5 사각형 넓이 : {ma.square_area(4, 5)}")   #출력: 20
---
+++ module_mymath_01.py
@@ -0,0 +1,9 @@
+# %%writefile (파일생성): 셀의 첫번째 줄에 있어야 함.(상단 주석도 안됨)
+
+mypi = 3.14
+
+def add(a, b):
+    return a + b
+
+def area(r):
+    return mypi * r * r
